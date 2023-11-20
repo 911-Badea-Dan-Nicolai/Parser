f09ea434df45a561d8dc312664f85b4b7791cb84
Catalin Albu <catalin.albu@exigea.ro>
2023-11-20 10:45:34 +0200

    Add grammar class

---
+++ src/Grammar.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;
+
+public class Grammar {
+    private Set<String> nonTerminals;
+    private Set<String> terminals;
+    private Map<String, List<String>> productions;
+
+    public Grammar() {
+        nonTerminals = new HashSet<>();
+        terminals = new HashSet<>();
+        productions = new HashMap<>();
+    }
+
+    public void readFromFile(String filename) throws IOException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            if (line.startsWith("States:")) {
+                String[] states = line.substring(8).split(", ");
+                nonTerminals.addAll(Arrays.asList(states));
+            } else if (line.startsWith("Alphabet:")) {
+                String[] alphabet = line.substring(10).split(", ");
+                terminals.addAll(Arrays.asList(alphabet));
+            } else if (line.startsWith("Transitions:")) {
+                while (scanner.hasNextLine()) {
+                    line = scanner.nextLine();
+                    if (line.isEmpty()) break;
+
+                    String[] parts = line.split("::=");
+                    String leftSide = parts[0].trim();
+                    String[] rightSideRules = parts[1].split("\\|");
+
+                    List<String> rulesList = productions.getOrDefault(leftSide, new ArrayList<>());
+                    Collections.addAll(rulesList, rightSideRules);
+                    productions.put(leftSide, rulesList);
+                }
+            }
+        }
+
+        scanner.close();
+    }
+
+    public void printNonTerminals() {
+        System.out.println("NonTerminals: " + nonTerminals);
+    }
+
+    public void printTerminals() {
+        System.out.println("Terminals: " + terminals);
+    }
+
+    public void printProductions() {
+        System.out.println("Productions:");
+        for (Map.Entry<String, List<String>> entry : productions.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+
+    public List<String> getProductionsForNonTerminal(String nonTerminal) {
+        return productions.get(nonTerminal);
+    }
+
+    public boolean isCFG() {
+        // Implement logic to check if the grammar is a CFG
+        return true;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner();
-        String program1 = "p1.txt";
-        String program1err = "p1err.txt";
-        String program2 = "p2.txt";
-        String program3 = "p3.txt";
-        scanner.scan(program1);
-        //scanner.scan(program1err);
-        scanner.scan(program2);
-        scanner.scan(program3);
-        //FA fa = new FA();
-        //String FAFile = "fa.in";
-        //fa.display(FAFile);
-
-        //String inputSequence = "0001";
-        //fa.isAccepted(inputSequence);
+        Grammar grammar = new Grammar();
+        grammar.readFromFile("src/program_inputs/g1.in");
+//        grammar.printTerminals();
+//        grammar.printProductions();
+//        grammar.printNonTerminals();
+//        System.out.println(grammar.getProductionsForNonTerminal("A"));
     }
 }
